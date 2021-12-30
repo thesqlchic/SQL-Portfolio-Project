bb796329de7b2988edc2f36f13472e62bb54caac
thesqlchic <95875234+thesqlchic@users.noreply.github.com>
2021-12-30 13:18:02 +0100

    Update Covid_project (SQL queries).sql

--- Covid_project (SQL queries).sql
+++ Covid_project (SQL queries).sql
@@ -1,3 +1,10 @@
+/*
+Covid 19 Data Exploration 
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
 select *
 from Covid_deaths
 where continent is not null
